db68bafdd7310746db1f00ad61bab860e2e1f89e
KudelinNikita <Rokotanskiy8@gmail.com>
2024-11-19 14:29:38 +0200

    Add files via upload

---
+++ Консоль 12 Куделин Н. 215гр.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Консоль_12_Куделин_Н._215гр
+{
+    class Program
+    {
+        static void Max(int a, int b)
+        {
+            //ЗАДАЧА 2.Вычислить значение выражения, определив и использовав дополнительный метод:
+
+            //static void IncrementVal(int x)
+            //{
+            //    x++;
+            //    Console.WriteLine($"IncrementVal:{x}");
+            //}
+
+            //ЗАДАЧА 3.Найти максимальную величину из двух целых переменных a, b с использованием вспомогательных методов.
+
+            //if (a > b)
+            //    return a;  
+            //else
+            //    return b;
+
+            //ЗАДАЧА 4.Найти максимальную величину их трех целых переменных a, b, c с использованием вспомогательных методов.
+
+            //{
+            //    if (a > b)
+            //        return a;  
+            //    else
+            //        return b;  
+            //}
+        }
+        //Метод задачи 2.
+        //static void Main()
+        //{
+        //    int a = 5;
+        //    Console.WriteLine($"Начальное значение переменной a={a}");
+        //    Console.WriteLine($"{a}");
+        //}
+
+        //Метод задачи 3.
+        //static void Main()
+        //{
+        //    int a = 10;
+        //    int b = 20;
+        //    int maxVal = Max(a, b);
+        //    Console.WriteLine("Максимальное значение: " + maxVal);
+        //}
+
+        //Метод задачи 4.
+        //static int MaxOfThree(int a, int b, int c)
+        //{
+        //    int maxAB = Max(a, b);
+        //    return Max(maxAB, c);
+        //}
+
+        //static void Main()
+        //{
+        //    int a = 10;
+        //    int b = 20;
+        //    int c = 15;
+        //    int maxVal = MaxOfThree(a, b, c);
+        //    Console.WriteLine("Максимальное значение из трех: " + maxVal);
+        //}
+
+
+    }
+}
